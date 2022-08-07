d3a608c7a902590a0e7cb6b21e71fac27ec68a7c
Alvaro Rocha <alvjeff@hotmail.com>
2022-08-07 16:25:41 -0400

    criação de arquivos html e js

---
+++ jogo.js
@@ -0,0 +1,18 @@
+// alert('teste entendido')
+
+var altura = 0
+var largura = 0
+
+function ajustaTamanhoPalcoJogo(){
+    altura = window.innerHeight //obs1
+    largura = window.innerWidth //obs1
+
+    console.log(altura, largura)
+}
+
+ajustaTamanhoPalcoJogo()
+
+
+
+
+/**ob1 - capturando largura e algura com essas funçoes nativas */
